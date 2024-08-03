4ba2b8e749ffeb93cf3bea8405401da8a98d283b
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-03 17:27:19 -0300

    configurando ambiente

---
+++ backend/src/server.js
@@ -0,0 +1,6 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
