61b86aadca0bfc5f01b9d716d56ad8d548a51bbd
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-05 11:37:27 -0300

    feat(pool): criando conexão e método de consulta

--- backend/src/app/database/pool.js
+++ backend/src/app/database/pool.js
@@ -3,8 +3,57 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class Pool {
-	constructor(){
-		this.pool = mysql2.createPool({});
+	constructor(
+		host = process.env.mysql_host || "localhost",
+		port = process.env.mysql_port || 3306,
+		user = process.env.mysql_user || "root",
+		password = process.env.mysql_pass || "",
+		database = process.env.mysql_db || "internship_project",
+		waitForConnections = true,
+		connectionLimit = 10,
+		maxIdle = 10,
+		idleTimeout = 60000,
+		queueLimit = 0,
+		enableKeepAlive = true,
+		keepAliveInitialDelay = 0
+	){
+		this.pool = mysql2.createPool({
+			host,
+			port,
+			user, 
+			password,
+			database,
+			waitForConnections,
+			connectionLimit,
+			maxIdle,
+			idleTimeout,
+			queueLimit,
+			enableKeepAlive,
+			keepAliveInitialDelay
+		});
+	}
+
+	getPool = () => this.pool;
+
+	/**
+	 * 
+	 * @param {string} sql SQL command string. 
+	 * @param {Array} values Array of values for the SQL query.
+	 * @param {*} errorMessage Error message (optional).
+	 * @returns Result of the query.
+	 */
+	async connection(sql, values, errorMessage){
+		let conn;
+		try{
+			conn = await this.getPool().promise().getConnection();
+			if(!conn) throw new Error("Couldn't get connection.");
+			const [ rows ] = await conn.execute(sql, values);
+			return rows;
+		}catch(e){
+			console.error(errorMessage, e);
+		}finally{
+			conn.release();
+		}
 	}
 }
 
