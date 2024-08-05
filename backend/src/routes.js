0af5c107e5e1ea8087efc16e3f87e1248e6b5e34
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-05 14:12:30 -0300

    test(routes.js): comentando e comentando o teste

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,10 @@ import pool from "./app/database/pool.js";
 
 const router = Router();
 
-router.get("/users", async (_req, res) => {
-	const result = await pool.connection("SELECT * FROM users;", [], "Couldn't get users");
+/* router.get("/users", async (_req, res) => {
+	const result = await pool.connection("SELECT * FROM employees;", [], "Couldn't get users");
 	console.log(result);
 	res.json(result);
-})
+}) */
 
 export default router;
