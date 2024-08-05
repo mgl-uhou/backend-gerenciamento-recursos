a3f39755f6bcba823eca501ac1b551f58ef93822
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-05 11:47:25 -0300

    test(routes/pool): testando a conexão com o banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,12 @@
 import { Router  } from "express";
+import pool from "./app/database/pool.js";
 
 const router = Router();
+
+router.get("/users", async (_req, res) => {
+	const result = await pool.connection("SELECT * FROM users;", [], "Couldn't get users");
+	console.log(result);
+	res.json(result);
+})
 
 export default router;
