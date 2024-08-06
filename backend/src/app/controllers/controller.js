6ffe950e3600c828bde1d256e8d3283d3e201ff9
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-06 10:53:13 -0300

    feat(store): Método que adiciona registros

--- backend/src/app/controllers/controller.js
+++ backend/src/app/controllers/controller.js
@@ -1,17 +1,17 @@
 import repository from "../repositories/repository.js";
 
-class Controller{
-	constructor(tableName, columns){
+class Controller {
+	constructor(tableName, columns) {
 		this._tableName = tableName;
 		this._columns = columns;
 	}
 	getTableName = () => this._tableName;
 	getColumns = () => this._columns;
 
-	async index(_req, res){
+	async index(_req, res) {
 		try {
 			const result = await repository.getAll(this.getTableName());
-			if(!result) throw new Error("Couldn't get data");
+			if (!result) throw new Error("Couldn't get data");
 
 			res.status(200).json(result);
 		} catch (e) {
@@ -19,12 +19,30 @@ class Controller{
 		}
 	}
 
-	async show(req, res){
+	async show(req, res) {
 		try {
-			const result = await repository.getById(this.getTableName(), req.params.id);
-			if(!result) throw new Error("Couldn't get data");
+			const result = await repository.getById(
+				this.getTableName(),
+				req.params.id
+			);
+			if (!result) throw new Error("Couldn't get data");
 
 			res.status(200).json(result);
+		} catch (e) {
+			res.status(400).json(e);
+		}
+	}
+
+	async store(req, res) {
+		try {
+			const result = await repository.insertRow(
+				this.getTableName(),
+				this.getColumns(),
+				Object.values(req.body)
+			);
+			if(!result) throw new Error (`Couldn't insert data into ${this.getTableName()}`);
+
+			res.status(201).json(result);
 		} catch (e) {
 			res.status(400).json(e);
 		}
