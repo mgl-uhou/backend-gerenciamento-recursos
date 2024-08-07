97de72f68b7a6924825d7e9bb9cccb038c6ada78
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-07 17:07:59 -0300

    feat(authMiddlare): Criando middleware de autenticação

---
+++ backend/src/app/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import repository from "../repositories/repository.js";
+
+export const authMiddleware = async (req, res, next) => {
+		try {
+			const { authorization } = req.headers;
+			
+			if(!authorization) throw new Error("Unauthorized.");
+
+			const token = authorization.split(" ")[1];
+
+			const { id } = jwt.verify(token, process.env.jwt_pass);
+
+			const users = await repository.getAll('employees');
+			let user;
+			for(let u of users)
+				if(u.id === id)
+					user = u;
+
+			if(!user) throw new Error("Unauthorized.");
+
+			const { password: _, ...loggedUser} = user;
+
+			req.user = loggedUser;
+
+			next();
+		} catch (e) {
+			res.status(401).json({ error: e.message });			
+		}	
+}
