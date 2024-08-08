64b687cc8699af300c456d13477344958a7247ce
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-08 13:26:43 -0300

    feat(routes-departments): Criação de rotas para departamentos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import employeeController from "./app/controllers/employeeController.js";
+import { departmentsController } from "./app/controllers/departmentsController.js";
 import { authMiddleware } from "./app/middlewares/authMiddleware.js";
 
 const router = Router();
@@ -17,5 +18,11 @@ router.post("/login", employeeController.login.bind(employeeController));
 router.put("/employees", authMiddleware, employeeController.updateSelf.bind(employeeController));
 router.put("/employees/:id", authMiddleware, employeeController.updateByAdmin.bind(employeeController));
 router.delete("/employees/:id", authMiddleware, employeeController.delete.bind(employeeController));
+
+router.get("/departments", authMiddleware, departmentsController.index.bind(departmentsController));
+router.get("/departments/:id", authMiddleware, departmentsController.show.bind(departmentsController));
+router.post("/departments", authMiddleware, departmentsController.store.bind(departmentsController));
+router.put("/departments/:id", authMiddleware, departmentsController.update.bind(departmentsController));
+router.delete("/departments/:id", authMiddleware, departmentsController.delete.bind(departmentsController));
 
 export default router;
