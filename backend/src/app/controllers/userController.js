79dd4a8d6943078a147c62da80ae14c4132a9ee7
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-08 12:38:59 -0300

    feat: separação de update de funcionário e administrador & proibição de atributos em store

--- backend/src/app/controllers/userController.js
+++ backend/src/app/controllers/userController.js
@@ -3,6 +3,7 @@ import Controller from "./controller.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 dotenv.config();
 
 class EmployeeController extends Controller{
@@ -12,10 +13,17 @@ class EmployeeController extends Controller{
 
 	async store(req, res) {
 		try {
-			const { email, password } = req.body;
+			const { email, password, vacationDays, available, isAdmin, departmentId, locationId } = req.body;
 			const users = await repository.getAll(this.getTableName());
 			const thereIsUser = users.some(user => user.email === email);
 			if(thereIsUser) throw new Error("User already exists");
+
+			if(vacationDays || available || isAdmin || departmentId || locationId){
+				return res.status(403).json({ 
+					error: "Forbidden: Unauthorized attributes.",
+					attributes: [ 'vacationDays', 'available', 'isAdmin', 'departmentId', 'locationId' ] 
+				})
+			}
 
 			const hashPassword = await bcrypt.hash(password, 10);
 			req.body.password = hashPassword;
@@ -26,9 +34,6 @@ class EmployeeController extends Controller{
 				Object.values(req.body)
 			);
 			if (!result) throw new Error(`Couldn't insert data into ${this.getTableName()}`);
-			
-			/* const { password: _, ...user } = result;
-			console.log(user); */
 			
 			res.status(201).json(result);
 		} catch (e) {
@@ -66,10 +71,87 @@ class EmployeeController extends Controller{
 
 	async getProfile(req, res){
 		try {
-			res.status(200).json(req.user);
+			res.status(200).json(req.employee);
 		} catch (e) {
 			res.status(401).json({ error: e.message });			
 		}	
+	}
+
+	/**
+	 * 
+	 * @param {Array} columns Array of column names.
+	 * @param {Array} values Array of values corresponding to the array of columns
+	 * @param {number} id Id's number of the record.
+	 * @returns Object with two properties: error and success.
+	 */
+	async update(columns, values, id){
+		const error = {};
+		const success = {};
+
+		for (const [index, column] of columns.entries()) {
+			// column is the value
+			try {
+				const result = await repository.updateRow(
+					this.getTableName(),
+					column,
+					values[index],
+					id
+				);
+				console.log(result);
+				
+				if (!result) throw new Error(`Could't update ${column}`);
+				success[column] = result;
+			} catch (e) {
+				error[column] = e.message;
+			}
+		}
+		return { error, success };
+	}
+
+	async updateSelf(req, res){
+		try {
+			const id = req.employee.id;
+			const element = await repository.getById(this.getTableName(), id);
+			if (!element.length) return res.status(404).json({ message: "Record not found" });
+
+			const { vacationDays, available, isAdmin, departmentId, locationId } = req.body;
+			if(vacationDays || available || isAdmin || departmentId || locationId){
+				return res.status(403).json({ 
+					error: "Forbidden: Unauthorized attributes.",
+					"attributes": [ 'vacationDays', 'available', 'isAdmin', 'departmentId', 'locationId' ] 
+				})
+			}
+
+			if(req.body.password) 
+				req.body.password = await bcrypt.hash(req.body.password, 10);
+
+			const columns = Object.keys(req.body);
+			const values = Object.values(req.body);
+
+			const { error, success } = await this.update(columns, values, id);
+			res.json({ success, error });
+		} catch (e) {
+			res.status(500).json({ error: "Error updating record", e });
+		}
+	}
+
+	async updateByAdmin(req, res){
+		try {
+			if(!req.employee.isAdmin) return res.status(401).json({ error: "Unauthorized."});
+
+			const recordId = req.params.id;
+			const element = await repository.getById(this.getTableName(), recordId);
+			if (!element.length) return res.status(404).json({ message: "Record not found" });
+
+			if(req.body.password) delete req.body.password;
+
+			const columns = Object.keys(req.body);
+			const values = Object.values(req.body);
+			const { error, success } = await this.update(columns, values, recordId);
+			res.json({ message: "Password  changed only by the employee on common route.", success, error });
+		} catch (e){
+			res.status(400).json({ error: "Error updating record", e });	
+		}
 	}
 }
 
