cc66660a40feef4555cc77ffce80fb6b9387ebe1
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-09 12:47:09 -0300

    fix: tirando middleware da rota post

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,21 +3,15 @@ import employeesController from "./app/controllers/employeesController.js";
 import { departmentsController } from "./app/controllers/departmentsController.js";
 import { locationsController } from "./app/controllers/locationsController.js";
 import { locationDepartmentsController } from "./app/controllers/locationDepartmentsController.js";
-import { vacationsController } from "./app/controllers/vacationsController.js";
 import { assignmentsController } from "./app/controllers/assignmentsController.js";
 import { authMiddleware } from "./app/middlewares/authMiddleware.js";
 
 const router = Router();
 
-/* router.get("/users", async (_req, res) => {
-	const result = await pool.connection("SELECT * FROM employees;", [], "Couldn't get users");
-	console.log(result);
-	res.json(result);
-}) */
 router.get("/employees",authMiddleware, employeesController.index.bind(employeesController));
 router.get("/employees/:id", authMiddleware, employeesController.show.bind(employeesController));
 router.get("/profile", authMiddleware, employeesController.getProfile.bind(employeesController));
-router.post("/employees", authMiddleware, employeesController.store.bind(employeesController));
+router.post("/employees", employeesController.store.bind(employeesController));
 router.post("/login", employeesController.login.bind(employeesController));
 router.put("/employees", authMiddleware, employeesController.updateSelf.bind(employeesController));
 router.put("/employees/:id", authMiddleware, employeesController.updateByAdmin.bind(employeesController));
@@ -40,12 +34,6 @@ router.get("/location-departments/:id", authMiddleware, locationDepartmentsContr
 router.post("/location-departments", authMiddleware, locationDepartmentsController.store.bind(locationDepartmentsController));
 router.put("/location-departments/:id", authMiddleware, locationDepartmentsController.update.bind(locationDepartmentsController));
 router.delete("/location-departments/:id", authMiddleware, locationDepartmentsController.delete.bind(locationDepartmentsController));
-
-router.get("/vacations", authMiddleware, vacationsController.index.bind(vacationsController));
-router.get("/vacations/:id", authMiddleware, vacationsController.show.bind(vacationsController));
-router.post("/vacations", authMiddleware, vacationsController.store.bind(vacationsController));
-router.put("/vacations/:id", authMiddleware, vacationsController.update.bind(vacationsController));
-router.delete("/vacations/:id", authMiddleware, vacationsController.delete.bind(vacationsController));
 
 router.get("/assignment", authMiddleware, assignmentsController.index.bind(assignmentsController));
 router.get("/assignment/:id", authMiddleware, assignmentsController.show.bind(assignmentsController));
