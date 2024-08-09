8d2e5424555cc9cbf07aa24be987f6cb9cd63203
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-08-09 13:17:46 -0300

    feat: Nova classe para usar o locationDepartmentRepository

--- backend/src/app/controllers/locationDepartmentsController.js
+++ backend/src/app/controllers/locationDepartmentsController.js
@@ -1,3 +1,25 @@
 import Controller from "./controller.js";
+import { locationDepartmentRepository } from "../repositories/locationDepartmentsRepository.js";
 
-export const locationDepartmentsController = new Controller('location_departments', [ "locationId", "departmentId", "minCapacity", "maxCapacity" ]);
+class LocationDepartmentController extends Controller {
+	constructor(tableName, columns) {
+		super(tableName, columns);
+	}
+
+	async index(req, res) {
+		try {
+			if (!req.employee.isAdmin) return this.unauthorized(res);
+			const result = await locationDepartmentRepository.getAll(this.getTableName());
+			if (!result) throw new Error("Couldn't get data");
+
+			res.status(200).json(result);
+		} catch (e) {
+			res.status(400).json(e);
+		}
+	}
+}
+
+export const locationDepartmentsController = new LocationDepartmentController(
+	"location_departments",
+	["locationId", "departmentId", "minCapacity", "maxCapacity"]
+);
